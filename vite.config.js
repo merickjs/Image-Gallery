74307fbbe3605972274318a4348d667f81d467c0
MericK <morhenac@gmail.com>
2023-09-25 22:49:26 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'import.meta.env.VITE_SOME_KEY': JSON.stringify(import.meta.env.VITE_SOME_KEY),
+  },
 })
